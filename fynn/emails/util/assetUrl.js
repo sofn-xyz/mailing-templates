da62ce5e737c4f62c4a9095bba89c41ba7e71bc8
Elliot Hursh <elliothursh@gmail.com>
2022-11-08 14:59:03 -0800

    default ASSET_URL

--- fynn/emails/util/assetUrl.js
+++ fynn/emails/util/assetUrl.js
@@ -1,10 +1,14 @@
+/*
+ * If you want to use local assets while developing new templates,
+ * comment out the following line.
+ *
+ * NOTE: This is cause the email linter to throw errors
+ */
+const ASSET_URL = "https://fynn-mailing.vercel.app";
+
 const assetUrl = (url) => {
-  if (url.startsWith("/")) {
-    if (process.env.ASSET_URL) {
-      return `${process.env.ASSET_URL}${url}`;
-    } else {
-      return `http://localhost:3883${url}`;
-    }
+  if (url.startsWith("/") && typeof ASSET_URL !== "undefined") {
+    return `${ASSET_URL}${url}`;
   }
 
   return url;
