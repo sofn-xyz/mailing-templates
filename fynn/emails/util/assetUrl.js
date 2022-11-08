c1864b6eb2429d5b338c238c2158db97905ce6c9
Elliot Hursh <elliothursh@gmail.com>
2022-11-08 15:09:58 -0800

    create separate command for local-assets

--- fynn/emails/util/assetUrl.js
+++ fynn/emails/util/assetUrl.js
@@ -1,17 +1,16 @@
 /*
  * If you want to use local assets while developing new templates,
- * comment out the following line.
+ * run `yarn dev:local-assets` instead of `yarn dev`
  *
  * NOTE: This will cause the email linter to throw errors
  */
+
 const ASSET_URL = "https://fynn-mailing.vercel.app";
 
-const assetUrl = (url) => {
-  if (url.startsWith("/") && typeof ASSET_URL !== "undefined") {
+export default function assetUrl(url) {
+  if (url.startsWith("/") && !process.env.LOCAL_ASSETS) {
     return `${ASSET_URL}${url}`;
   }
 
   return url;
-};
-
-export default assetUrl;
+}
